bd218f1be8b1782bc094e57da21257897a878822
Pranay4921 <pranay_uniyal@yahoo.com>
2022-01-25 16:07:07 +0530

    Addition of user interface
    Addition of Sliders and Integrator Switch button

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ let theta1 = 1.5 * 3.1415926; theta2 = 0;
 let omega1 = 0; omega2 = 0;
 let dt = 0.01;
 
+intgrt_str = 'Intergrator is Euler';
+intgrt_bool = 1;
+
 
 function f_fun(t1, t2, o1, o2) {
     temp1 = g * (2 * m1 + m2) * sin(t1);
@@ -21,12 +24,99 @@ function g_fun(t1, t2, o1, o2) {
     return 2 * sin(t1 - t2) * (temp1 + temp2 + temp3) / (L2 * temp4);
 }
 
-function setup() {
-    createCanvas(800, 400);
+function eulermethod() {
+    //Euler Method
+    tem1 = theta1 + dt * omega1;
+    tem2 = theta2 + dt * omega2;
+    tem3 = omega1 + dt * f_fun(theta1, theta2, omega1, omega2);
+    tem4 = omega2 + dt * g_fun(theta1, theta2, omega1, omega2);
+    theta1 = tem1;
+    theta2 = tem2;
+    omega1 = tem3;
+    omega2 = tem4;
+}
+
+function RK4() {
+    //RungeKutta 4th order
+    k1 = [0, 0, 0, 0]; k2 = [0, 0, 0, 0]; k3 = [0, 0, 0, 0]; k4 = [0, 0, 0, 0];
+    k1[1] = dt * omega1;
+    k1[2] = dt * omega2;
+    k1[3] = dt * f_fun(theta1, theta2, omega1, omega2);
+    k1[4] = dt * g_fun(theta1, theta2, omega1, omega2);
+
+    k2[1] = dt * (omega1 + k1[3] / 2);
+    k2[2] = dt * (omega2 + k1[4] / 2);
+    k2[3] = dt * f_fun(theta1 + k1[1] / 2, theta2 + k1[2] / 2, omega1 + k1[3] / 2, omega2 + k1[4] / 2);
+    k2[4] = dt * g_fun(theta1 + k1[1] / 2, theta2 + k1[2] / 2, omega1 + k1[3] / 2, omega2 + k1[4] / 2);
+
+    k3[1] = dt * (omega1 + k2[3] / 2);
+    k3[2] = dt * (omega2 + k2[4] / 2);
+    k3[3] = dt * f_fun(theta1 + k2[1] / 2, theta2 + k2[2] / 2, omega1 + k2[3] / 2, omega2 + k2[4] / 2);
+    k3[4] = dt * g_fun(theta1 + k2[1] / 2, theta2 + k2[2] / 2, omega1 + k2[3] / 2, omega2 + k2[4] / 2);
+
+    k4[1] = dt * (omega1 + k3[3]);
+    k4[2] = dt * (omega2 + k3[4]);
+    k4[3] = dt * f_fun(theta1 + k3[1], theta2 + k3[2], omega1 + k3[3], omega2 + k3[4]);
+    k4[4] = dt * g_fun(theta1 + k3[1], theta2 + k3[2], omega1 + k3[3], omega2 + k3[4]);
+
+    theta1 = theta1 + (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]) / 6;
+    theta2 = theta2 + (k1[2] + 2 * k2[2] + 2 * k3[2] + k4[2]) / 6;
+    omega1 = omega1 + (k1[3] + 2 * k2[3] + 2 * k3[3] + k4[3]) / 6;
+    omega2 = omega2 + (k1[4] + 2 * k2[4] + 2 * k3[4] + k4[4]) / 6;
 
 }
 
+function switchIntgrt() {
+    if (intgrt_bool == 1) {
+        intgrt_bool = 0;
+        intgrt_str = 'Integrator is Runge Kutta-4';
+        intgrt_para.html(intgrt_str);
+    } else {
+        intgrt_bool = 1;
+        intgrt_str = 'Integrator is Euler';
+        intgrt_para.html(intgrt_str);
+    }
+}
+
+
+function resetSketch() {
+    createCanvas(800, 400);
+    dt = slider_time.value() / 1000;
+    L1 = slider_L1.value();
+    L2 = slider_L2.value();
+    theta1 = slider_theta1.value() / 1000;
+    theta2 = slider_theta2.value() / 1000;
+    omega1 = 0;
+    omega2 = 0;
+
+}
+
+function setup() {
+    createCanvas(800, 400);
+    createP('TimeStep (0.005 - 0.9)');
+    slider_time = createSlider(5, 900, 10);
+    createP('L1 (50 - 100)');
+    slider_L1 = createSlider(50, 100, 75);
+    createP('L2 (50 - 100)');
+    slider_L2 = createSlider(50, 100, 75);
+    createP('theta1 (0 - 2PI)');
+    slider_theta1 = createSlider(0, 6280, 4710);
+    createP('theta2 (0 - 2PI)');
+    slider_theta2 = createSlider(0, 6280, 0);
+
+
+    intgrt_para = createP(intgrt_str);
+    intgrt_btn = createButton('Switch Integrator')
+    intgrt_btn.mousePressed(switchIntgrt);
+
+
+    createP('');
+    var reset_btn = createButton("Apply Changes");
+    reset_btn.mousePressed(resetSketch);
+}
+
 function draw() {
+
     fill(200);
     let mar = width / 100;
     rect(0, 0, width / 2, height);
@@ -52,29 +142,32 @@ function draw() {
     the2 = theta2;
     if (the1 > 2 * PI) {
         the1 = the1 - 2 * PI;
-    }
-    if (the1 < 0) {
+    } else if (the1 < 0) {
         the1 = the1 + 2 * PI;
+    } else {
+        the1 = the1;
     }
+
+
     if (the2 > 2 * PI) {
         the2 = the2 - 2 * PI;
-    }
-    if (the2 < 0) {
+    } else if (the2 < 0) {
         the2 = the2 + 2 * PI;
+    } else {
+        the2 = the2;
     }
+
     let x_theta1 = width / 2 + mar + ((width / 2 - 2 * mar) / 7) * the1;
     let y_theta1 = height - mar - ((height - 2 * mar) / 7) * the2;
     circle(x_theta1, y_theta1, height / 200);
 
+    if (intgrt_str == 1) {
+        eulermethod();
+    } else {
+        RK4();
+    }
 
-    //Euler Method
-    tem1 = theta1 + dt * omega1;
-    tem2 = theta2 + dt * omega2;
-    tem3 = omega1 + dt * f_fun(theta1, theta2, omega1, omega2);
-    tem4 = omega2 + dt * g_fun(theta1, theta2, omega1, omega2);
-    theta1 = tem1;
-    theta2 = tem2;
-    omega1 = tem3;
-    omega2 = tem4;
+
+
 
 }
